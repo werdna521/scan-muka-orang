1736110f5ec30bc3982af0f00fa231a41eb7e09d
Andrew Cen <andrew.cen@binus.ac.id>
2019-09-30 15:56:16 +0700

    update pojo class

--- app/src/main/java/android/bearcatsdev/mat/io19/scanner/pojo/Participant.kt
+++ app/src/main/java/android/bearcatsdev/mat/io19/scanner/pojo/Participant.kt
@@ -1,5 +1,8 @@
 package android.bearcatsdev.mat.io19.scanner.pojo
 
+import com.squareup.moshi.Json
+
 data class Participant(
-    val name: String
+    @field:Json(name = "name") val name: String,
+    @field:Json(name = "nim") val nim: String
 )
