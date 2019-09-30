b5b05eeaa4ba14b8645b902a794e1af56a36b6d9
Andrew Cen <andrew.cen@binus.ac.id>
2019-09-30 22:07:06 +0700

    added kotlin coroutine and android lifecycle library dependencies

---
+++ app/src/main/java/android/bearcatsdev/mat/io19/scanner/viewmodel/ParticipantViewModel.kt
@@ -0,0 +1,14 @@
+package android.bearcatsdev.mat.io19.scanner.viewmodel
+
+import android.bearcatsdev.mat.io19.scanner.pojo.Participant
+import android.bearcatsdev.mat.io19.scanner.repository.ParticipantRepository
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class ParticipantViewModel: ViewModel() {
+    private val repository = ParticipantRepository()
+
+//    fun doSignUp(): LiveData<Participant> {
+//        return
+//    }
+}
