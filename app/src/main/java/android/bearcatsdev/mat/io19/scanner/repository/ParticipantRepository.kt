56b6f7ccdd404001d7d58fef0ed609e28205016d
Andrew Cen <andrew.cen@binus.ac.id>
2019-09-30 20:24:48 +0700

    created repository layer to separate view model and retrofit service

---
+++ app/src/main/java/android/bearcatsdev/mat/io19/scanner/repository/ParticipantRepository.kt
@@ -0,0 +1,9 @@
+package android.bearcatsdev.mat.io19.scanner.repository
+
+import android.bearcatsdev.mat.io19.scanner.services.ParticipantService
+
+class ParticipantRepository {
+    private val participantService = ParticipantService.getService()
+
+    suspend fun signUp() = participantService.signUp()
+}
