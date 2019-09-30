b6529294f305b25d25c3516a95d1e7291baf2c2c
Andrew Cen <andrew.cen@binus.ac.id>
2019-09-30 21:56:58 +0700

    update service endpoint and json request body

--- app/src/main/java/android/bearcatsdev/mat/io19/scanner/repository/ParticipantRepository.kt
+++ app/src/main/java/android/bearcatsdev/mat/io19/scanner/repository/ParticipantRepository.kt
@@ -5,5 +5,5 @@ import android.bearcatsdev.mat.io19.scanner.services.ParticipantService
 class ParticipantRepository {
     private val participantService = ParticipantService.getService()
 
-    suspend fun signUp() = participantService.signUp()
+    suspend fun signUp(qrCode: String) = participantService.signUp(qrCode)
 }
--- app/src/main/java/android/bearcatsdev/mat/io19/scanner/services/ParticipantService.kt
+++ app/src/main/java/android/bearcatsdev/mat/io19/scanner/services/ParticipantService.kt
@@ -2,8 +2,10 @@ package android.bearcatsdev.mat.io19.scanner.services
 
 import android.bearcatsdev.mat.io19.scanner.pojo.Participant
 import android.bearcatsdev.mat.io19.scanner.utils.Constants
+import com.squareup.moshi.Json
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Field
 import retrofit2.http.POST
 
 interface ParticipantService {
@@ -19,5 +21,5 @@ interface ParticipantService {
     }
 
     @POST("api/v1/signup")
-    suspend fun signUp(): Participant
+    suspend fun signUp(@Field("qr_hash") qrCode: String): Participant
 }
